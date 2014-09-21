225a867a3441cc8242e6fc4289b0a1feea83deb4
Bryan Herman <bherman@mit.edu>
2014-09-20 21:17:16 -0400

    added base files that compile

---
+++ src/constants.F90
@@ -0,0 +1,35 @@
+module constants
+
+  implicit none
+
+  ! SlowingDown major, minor, and release numbers
+  integer, parameter :: VERSION_MAJOR   = 0
+  integer, parameter :: VERSION_MINOR   = 0
+  integer, parameter :: VERSION_RELEASE = 0
+
+  ! Maximum number of words in a single line, length of line, and length of
+  ! single word
+  integer, parameter :: MAX_WORDS    = 500
+  integer, parameter :: MAX_LINE_LEN = 250
+  integer, parameter :: MAX_WORD_LEN = 150
+  integer, parameter :: MAX_FILE_LEN = 255
+
+  ! ============================================================================
+  ! PHYSICAL CONSTANTS
+
+  ! Values here are from the Committee on Data for Science and Technology
+  ! (CODATA) 2010 recommendation (doi:10.1103/RevModPhys.84.1527).
+
+  real(8), parameter ::            &
+       PI           = 3.1415926535898_8, & ! pi
+       MASS_NEUTRON = 1.008664916,       & ! mass of a neutron in amu
+       MASS_PROTON  = 1.007276466812,    & ! mass of a proton in amu
+       AMU          = 1.660538921e-27,   & ! 1 amu in kg
+       N_AVOGADRO   = 0.602214129,       & ! Avogadro's number in 10^24/mol
+       K_BOLTZMANN  = 8.6173324e-11,     & ! Boltzmann constant in MeV/K
+       INFINITY     = huge(0.0_8),       & ! positive infinity
+       ZERO         = 0.0_8,             &
+       ONE          = 1.0_8,             &
+       TWO          = 2.0_8
+
+end module constants
---
+++ src/error.F90
@@ -0,0 +1,136 @@
+module error
+
+  use, intrinsic :: ISO_FORTRAN_ENV
+
+  use constants,  only: MAX_LINE_LEN
+
+  implicit none
+
+  ! saved module variables
+  character(len=MAX_LINE_LEN), save :: message
+
+contains
+
+!===============================================================================
+! WARNING issues a warning to the user in the log file and the standard output
+! stream.
+!===============================================================================
+
+  subroutine warning()
+
+    integer :: i_start   ! starting position
+    integer :: i_end     ! ending position
+    integer :: line_wrap ! length of line
+    integer :: length    ! length of message
+    integer :: indent    ! length of indentation
+
+    ! Write warning at beginning
+    write(ERROR_UNIT, fmt='(1X,A)', advance='no') 'WARNING: '
+
+    ! Set line wrapping and indentation
+    line_wrap = 80
+    indent = 10
+
+    ! Determine length of message
+    length = len_trim(message)
+
+    i_start = 0
+    do
+      if (length - i_start < line_wrap - indent + 1) then
+        ! Remainder of message will fit on line
+        write(ERROR_UNIT, fmt='(A)') message(i_start+1:length)
+        exit
+
+      else
+        ! Determine last space in current line
+        i_end = i_start + index(message(i_start+1:i_start+line_wrap-indent+1), &
+             ' ', BACK=.true.)
+
+        if (i_end == i_start) then
+          ! This is a special case where there is no space
+          i_end = i_start + line_wrap - indent + 1
+          write(ERROR_UNIT, fmt='(A/A)', advance='no') &
+               message(i_start+1:i_end-1), repeat(' ', indent)
+          i_end = i_end - 1
+        else
+          ! Write up to last space
+          write(ERROR_UNIT, fmt='(A/A)', advance='no') &
+               message(i_start+1:i_end-1), repeat(' ', indent)
+        end if
+
+        ! Advance starting position
+        i_start = i_end
+        if (i_start > length) exit
+      end if
+    end do
+
+  end subroutine warning
+
+!===============================================================================
+! FATAL_ERROR alerts the user that an error has been encountered and displays a
+! message about the particular problem. Errors are considered 'fatal' and hence
+! the program is aborted.
+!===============================================================================
+
+  subroutine fatal_error()
+
+    integer :: code      ! error code
+    integer :: i_start   ! starting position
+    integer :: i_end     ! ending position
+    integer :: line_wrap ! length of line
+    integer :: length    ! length of message
+    integer :: indent    ! length of indentation
+
+    ! Write error at beginning
+    write(ERROR_UNIT, fmt='(1X,A)', advance='no') 'ERROR: '
+
+    ! Set line wrapping and indentation
+    line_wrap = 80
+    indent = 8
+
+    ! Determine length of message
+    length = len_trim(message)
+
+    i_start = 0
+    do
+      if (length - i_start < line_wrap - indent + 1) then
+        ! Remainder of message will fit on line
+        write(ERROR_UNIT, fmt='(A)') message(i_start+1:length)
+        exit
+
+      else
+        ! Determine last space in current line
+        i_end = i_start + index(message(i_start+1:i_start+line_wrap-indent+1), &
+             ' ', BACK=.true.)
+
+        if (i_end == i_start) then
+          ! This is a special case where there is no space
+          i_end = i_start + line_wrap - indent + 1
+          write(ERROR_UNIT, fmt='(A/A)', advance='no') &
+               message(i_start+1:i_end-1), repeat(' ', indent)
+          i_end = i_end - 1
+        else
+          ! Write up to last space
+          write(ERROR_UNIT, fmt='(A/A)', advance='no') &
+               message(i_start+1:i_end-1), repeat(' ', indent)
+        end if
+
+        ! Advance starting position
+        i_start = i_end
+        if (i_start > length) exit
+      end if
+    end do
+
+    ! Release memory from all allocatable arrays
+!   call free_memory()
+
+    ! Abort program
+#ifdef NO_F2008
+    stop
+#else
+    error stop 
+#endif
+
+  end subroutine fatal_error
+
+end module error
